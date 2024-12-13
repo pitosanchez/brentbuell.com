c2e1bb9b8d23867286154fb01cbbc1c697ed966f
Robert Pito Sanchez <robsanchez124@gmail.com>
2024-12-12 21:42:43 -0500

    added img of brent, add paragraph, adjusted content, add dynamic words

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
 import "./style.css";
+
+// Dynamic Words Animation
+const words: string[] = [
+  "PRODUCER",
+  "NOVELIST",
+  "PLAYWRIGHT",
+  "DIRECTOR",
+  "PHOTOGRAPHER",
+  "PUBLIC SPEAKER",
+];
+let currentIndex: number = 0;
+const dynamicWordsElement = document.getElementById(
+  "dynamic-words"
+) as HTMLElement | null;
+
+function updateWord(): void {
+  if (dynamicWordsElement) {
+    dynamicWordsElement.textContent = words[currentIndex];
+    currentIndex = (currentIndex + 1) % words.length;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateWord();
+  setInterval(updateWord, 2000);
+});
