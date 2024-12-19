7fe00d556298d919626dece8889767252d39b9bb
Robert Pito Sanchez <robsanchez124@gmail.com>
2024-12-18 22:54:26 -0500

    made structural changes to the home html, css and add vite config file

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve, join } from "node:path";
+import { cwd } from "node:process";
+import { globSync } from "node:fs";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [],
+  root: join(cwd(), "src"),
+  publicDir: join(cwd(), "public"),
+  build: {
+    rollupOptions: {
+      input: globSync(resolve(cwd(), "src", "**/*.html")),
+    },
+    outDir: join(cwd(), "dist"),
+  },
+});
